3f649440763eef19c97ec6ae333f00e022cccbf1
nabilhayet <nabil4457@yahoo.com>
2020-09-23 14:24:59 -0400

    refactor food controller

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,25 +1,33 @@
 class FoodsController < ApplicationController
 
     def index
-        if is_logged_in?
-          @admin = current_user
-          if params[:admin_id]
-            @foods = Admin.find(params[:admin_id]).foods 
-          else
-            @foods = @admin.foods 
-          end
-        else 
-            redirect_to admin_login_path 
-        end 
+      if is_logged_in?
+        if params[:admin_id]
+          @admin = Admin.find_by(id: params[:admin_id])
+          if @admin.nil?
+            redirect_to admin_profile_path(@admin), alert: "Admin not found."
+          else 
+            if @admin != current_user
+               redirect_to admin_profile_path(current_user)
+            else 
+                @foods = current_user.foods
+            end 
+          end 
+        else
+          @foods = current_user.foods
+        end
+      else 
+          redirect_to admin_login_path 
       end
-  
+    end 
+
       def show 
         if is_logged_in?
           @admin = current_user
           @food = Food.find_by(params[:id])
-          if @food.admin.id != @admin.id 
+            if @food.admin != @admin
               redirect_to admin_profile_path(@admin)
-          end 
+            end 
         else 
           redirect_to admin_login_path 
         end 
@@ -27,12 +35,25 @@ class FoodsController < ApplicationController
   
       def new 
         if is_logged_in?
-          @admin = current_user 
-          @food = @admin.foods.build
+          if params[:admin_id]
+            @admin = Admin.find_by(id: params[:admin_id])
+            if @admin.nil?
+              redirect_to admin_profile_path(current_user), alert: "Admin not found."
+            else 
+              if @admin != current_user
+                 redirect_to admin_profile_path(current_user)
+              else 
+                @admin = current_user
+                @food = @admin.foods.build
+              end 
+            end 
+          else 
+            admin_profile_path(current_user)
+          end 
         else 
           redirect_to admin_login_path 
         end 
-      end 
+      end
   
       def create
           @admin = current_user 
@@ -49,7 +70,7 @@ class FoodsController < ApplicationController
           if params[:admin_id]
             @admin = Admin.find_by(id: params[:admin_id])
             if @admin.nil?
-              redirect_to admin_profile_path(@admin), alert: "Admin not found."
+              redirect_to admin_profile_path(current_user), alert: "Admin not found."
             else
               @food = @admin.foods.find_by(id: params[:id])
               redirect_to admin_foods_path(@admin), alert: "Food not found." if @food.nil?
@@ -57,32 +78,51 @@ class FoodsController < ApplicationController
           else
             @admin = current_user
             @food = Food.find(params[:id])
-              if @food.admin != @admin
-                redirect_to admin_profile_path(@admin)
-              end 
+            if @food.admin != @admin
+              redirect_to admin_profile_path(@admin)
+            end 
           end
         else 
-          redirect_to admin_login_path 
+          redirect_to admin_login_path
         end 
       end
   
         def update
           @admin = current_user 
-          @food = Food.find(params[:id])
+          @food = Food.find_by_id(params[:id])
           @food.update(food_params)
           redirect_to admin_food_path(@admin, @food)
         end
-  
-      def destroy 
-        if is_logged_in?
-          @admin = current_user 
-          @food = Food.find(params[:id])
-          @food.delete
-          redirect_to admin_foods_path(@admin)
-        else 
-          redirect_to admin_login_path
+
+        def destroy 
+          if is_logged_in?
+            if params[:admin_id]
+              @admin = Admin.find_by(id: params[:admin_id])
+              if @admin.nil?
+                redirect_to admin_profile_path(current_user), alert: "Admin not found."
+              else
+                @food = @admin.foods.find_by(id: params[:id])
+                if @food.nil?
+                  redirect_to admin_foods_path(@admin), alert: "Food not found."
+                else 
+                  @food.delete
+                  redirect_to admin_foods_path(@admin)
+                end 
+              end 
+            else
+              @admin = current_user
+              @food = Food.find(params[:id])
+                if @food.admin != @admin
+                  redirect_to admin_profile_path(@admin)
+                else 
+                  @food.delete
+                  redirect_to admin_foods_path(@admin)
+                end 
+            end 
+         else 
+            redirect_to admin_login_path
+          end 
         end 
-      end 
   
    private
 
